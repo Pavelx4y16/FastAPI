3dca5a0db402deaea5fa8abe2ac24a3931c27ddf
Pavel Savastseika <savaspavel@gmail.com>
2023-04-24 16:29:28 +0400

    Add dynamic route with 'id' parameter

---
+++ main.py
@@ -0,0 +1,13 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+
+@app.get('/')
+def index():
+    return {'data': "Blog List"}
+
+
+@app.get('/blog/{id}')
+def show_blog(id):
+    return {'blog': f"{id}"}
