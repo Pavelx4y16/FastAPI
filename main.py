6667138851fa81a8aa2f48ed398a7452a0dbe2ed
Pavel Savastseika <savaspavel@gmail.com>
2023-04-25 12:34:35 +0400

    Add one more dynamic route

--- main.py
+++ main.py
@@ -8,6 +8,14 @@ def index():
     return {'data': "Blog List"}
 
 
+@app.get('/blog/{id}/comments')
+def show_blog_comments(id: int):
+    return {
+        'blog_id': id,
+        'comments': "this blog has some comments."
+    }
+
+
 @app.get('/blog/{id}')
 def show_blog(id: int):
     return {'blog': id}
